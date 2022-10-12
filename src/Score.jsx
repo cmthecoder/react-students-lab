682e75754c3dfe1e7051fe31a65c3b648184719f
Chris Mems <christopher.mems@.com>
2022-10-12 11:32:57 -0500

    move data into data.js

--- src/Score.jsx
+++ src/Score.jsx
@@ -1,10 +1,10 @@
 const Score = (props) => {
   console.log(props.student.date)
   return (
-    <>
+    <div>
       <h2>{props.student.date}</h2>
       <h2>{props.student.score}</h2>
-    </>
+    </div>
   );
 }
 
--- src/Student.jsx
+++ src/Student.jsx
@@ -3,13 +3,13 @@ import Score from "./Score";
 const Student = (props) => {
   console.log(props.student.scores)
   return (
-    <>
+    <div>
       <h1>{props.student.name}</h1>
       <h2>{props.student.bio}</h2>
       {props.student.scores.map(studentEl => {
         return <Score key = {studentEl.score} student = {studentEl}  />
       })}
-    </>
+    </div>
   );
 }
 
